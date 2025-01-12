52f062d54e9cafb5a0eec7232a6978904f73a24e
Subham11258 <subhamguptaslg.1996@gmail.com>
2025-01-12 16:25:58 +0530

    first commit

---
+++ src/data/user_info.js
@@ -0,0 +1,116 @@
+const info = {
+    // ============ MAIN DETAILS ============
+    main: {
+        name: "Subham Gupta",
+        description:"Aspiring Frontend/MERN Stack Developer with expertise in JavaScript, React, Node.js, and a passion for building dynamic, user-friendly web applications. I’m currently enrolled in Scaler Academy, where I’ve honed my skills in DSA, SQL, JavaScript, React, and the MERN stack, and cleared multiple mock interviews, including React and MERN. I hold certifications in React, JavaScript, Problem Solving, Frontend Python, and SQL from HackerRank, along with a HackerRank React Certification, showcasing my strong technical foundation. I’ve built impactful projects like a bookings website using the MERN stack with Stripe payment integration and data encryption, an IMDb clone, and a Google Gemini clone. Additionally, I’ve contributed to a collaborative laptop rental service for students, leveraging Django, PostgreSQL, and React. With a strong problem-solving mindset and a focus on continuous learning, I’m dedicated to enhancing both my technical and communication skills to excel in collaborative environments.",
+        role: "Full-Stack Developer",
+        photo: "../../public/profile.jpeg",
+        email: "subhamguptaslg.1996@gmail.com",
+    },
+
+    // ============ SOCIAL LINKS ============
+    socials: {
+        twitter: "https://twitter.com/SubhamGGG",
+		github: "https://github.com/Subham11258",
+		linkedin: "https://linkedin.com/in/sg11258",
+		instagram: "https://instagram.com/subhamguptta_",
+		facebook: "https://www.facebook.com/profile.php?id=100072225739697",
+    },
+
+    // ============ PROJECTS ============
+    projects: [
+        {
+			title: "MyBookings",
+			description: "A bookings application built with the MERN stack, featuring Stripe payment integration, data encryption, and a responsive user interface, enabling secure transactions and seamless management of bookings for users.",
+            technologies: "React, Ant Design, Node.js, Express, Javascript, MongoDB, Mongoose, Stripe, JWT, Axios, Redux",
+			github: "https://github.com/Subham11258/mybookings",
+            link: "https://mybookings-frontend.vercel.app/"
+		},
+
+        {
+			title: "My Open Book",
+			description: "A social media application built with the MERN stack, offering user authentication, real-time posts, likes, comments, and a responsive UI, enabling seamless interaction and engagement among users.",
+			technologies: "React, Tailwind, Node.js, Express, Javascript, MongoDB, Formik, Mongoose, Context",
+			github: "https://github.com/Subham11258/social-media-main",
+            link: "https://social-media-main-kappa.vercel.app/"
+		},
+
+        {
+			title: "IMDB",
+			description: "An IMDb clone built with React, Vite, and Tailwind CSS, featuring a watchlist, pagination, and data fetched from the TMDB API, offering an intuitive interface for browsing movies.",
+			technologies: "React, Tailwind, Javascript, TMDB, Vite",
+			github: "https://github.com/Subham11258/IMBD-clone",
+            link: "https://imbd-clone-subham.vercel.app/"
+		},
+
+		// {
+		// 	title: "Signature Application",
+		// 	description: "A signature application built with HTML, CSS, and JavaScript, featuring text color pickers, background selection, font adjustments, and options to save, download, or retrieve signatures for personalized use.",
+		// 	technologies: "Javascript",
+		// 	github: "https://github.com/Subham11258/Signature-Application",
+        //     link: "https://signature-application-1.vercel.app/"
+		// },
+
+    ],
+
+    // ============ EDUCATION ============
+    education: [
+        {
+            school: "Scaler Academy",
+            degree: "Specialization in Software Development and Problem Solving",
+            duration: "2024-Present",
+            image: "scaler-logo.png"
+        },
+        {
+            school: "Lovely Professional University",
+            degree: "Bachelors of Computer Application",
+            duration: "2019-2022",
+            image: "lpu-logo.png"
+        },
+    ],
+
+    // ============ EXPERIENCE ============
+    experience: [
+       
+    ],
+
+    // ============ CERTIFICATES ============
+    certificates: [
+        {
+            title: "Certified Frontedend Developer (React)",
+            description: "Provided by HackerRank",
+            icon: "hackerrank-logo.webp",
+            link: "https://shorturl.at/Y3y3c",
+        },
+        {
+            title: "Certified Software Engineer Intern",
+            description: "Provided by HackerRank",
+            icon: "hackerrank-logo.webp",
+            link: "https://shorturl.at/OeGoS",
+        },
+        {
+            title: "Certified at Javascript (Intermediate)",
+            description: "Provided by HackerRank",
+            icon: "hackerrank-logo.webp",
+            link: "https://shorturl.at/O5Cub"
+        },
+        {
+            title: "Certified at Problem Solving (Intermediate)",
+            description: "Provided by HackerRank",
+            icon: "hackerrank-logo.webp",
+            link: "https://shorturl.at/SUM0Y"
+        },
+        
+    ],
+
+    // ============ CONTACT ============
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at subhamguptaslg.1996@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    // ============ FOOTER ============
+    footer: "© 2024 Subham Gupta. All Rights Reserved"
+}
+
+export default info
